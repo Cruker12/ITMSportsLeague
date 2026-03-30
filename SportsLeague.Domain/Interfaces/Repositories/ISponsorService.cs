09b54161533335b2469627d6b45cfbc1011bb9ff
Cruker12 <Crukerd12@gmail.com>
2026-03-30 17:15:12 -0500

    Add Interfaces (ISponsor-ISponsorService-ITournamentSponsorRepository)

---
+++ SportsLeague.Domain/Interfaces/Repositories/ISponsorService.cs
@@ -0,0 +1,17 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Services;
+
+public interface ISponsorService
+{
+    Task<IEnumerable<Sponsor>> GetAllAsync();
+    Task<Sponsor?> GetByIdAsync(int id);
+    Task<Sponsor> CreateAsync(Sponsor sponsor);
+    Task UpdateAsync(int id, Sponsor sponsor);
+    Task DeleteAsync(int id);
+
+    // Relación N:M con Tournament
+    Task<TournamentSponsor> RegisterSponsorToTournamentAsync(int sponsorId, int tournamentId, decimal contractAmount);
+    Task RemoveSponsorFromTournamentAsync(int sponsorId, int tournamentId);
+    Task<IEnumerable<TournamentSponsor>> GetTournamentsBySponsorAsync(int sponsorId);
+}

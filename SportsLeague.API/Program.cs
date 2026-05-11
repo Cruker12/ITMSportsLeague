259d67f0e8cbb3b625ac50e1d74152d1eb390afb
Cruker12 <Crukerd12@gmail.com>
2026-05-11 18:15:50 -0500

    [Phase 4] Create the MatchController and Update Program.cs

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -20,10 +20,9 @@ builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
 builder.Services.AddScoped<IRefereeRepository, RefereeRepository>();
 builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
 builder.Services.AddScoped<ITournamentTeamRepository, TournamentTeamRepository>();
-// ── ──
 builder.Services.AddScoped<ISponsorRepository, SponsorRepository>();
 builder.Services.AddScoped<ITournamentSponsorRepository, TournamentSponsorRepository>();
-// ── ──
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
 
 
 // ── Services ──
@@ -31,10 +30,8 @@ builder.Services.AddScoped<ITeamService, TeamService>();
 builder.Services.AddScoped<IPlayerService, PlayerService>();
 builder.Services.AddScoped<IRefereeService, RefereeService>();
 builder.Services.AddScoped<ITournamentService, TournamentService>();
-// ──  ──
 builder.Services.AddScoped<ISponsorService, SponsorService>();
-// ──  ──
-
+builder.Services.AddScoped<IMatchService, MatchService>();
 
 // ── AutoMapper ──
 builder.Services.AddAutoMapper(typeof(Program).Assembly);

91331977210ad286eba0738655dee8b8eeb0fcc4
Cruker12 <Crukerd12@gmail.com>
2026-05-18 13:28:57 -0500

    [Assessment Point 4] Create DTOs (CreateMatchLineupDto and MatchLineupDto)

---
+++ SportsLeague.API/DTOs/Request/CreateMatchLineupDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsLeague.API.DTOs.Request;
+
+public class CreateMatchLineupDto
+{
+    [Required]
+    public int PlayerId { get; set; }
+
+    [Required]
+    public bool IsStarter { get; set; }
+
+    [Required]
+    [MaxLength(10)]
+    public string Position { get; set; } = string.Empty;   // "GK", "CB", "CDM", "ST", etc.
+}
---
+++ SportsLeague.API/DTOs/Response/MatchLineupDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsLeague.API.DTOs.Response;
+
+public class MatchLineupDto
+{
+    public int Id { get; set; }
+    public int MatchId { get; set; }
+    public int PlayerId { get; set; }
+    public string PlayerName { get; set; } = string.Empty;   // FirstName + " " + LastName
+    public string TeamName { get; set; } = string.Empty;     // Nombre del equipo del jugador (name of the player's team)
+    public bool IsStarter { get; set; }
+    public string Position { get; set; } = string.Empty;
+}
